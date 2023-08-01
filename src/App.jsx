88ae51d9d63514aae12a12f52971c1576318f1b8
Kristjan Lääne <xonproject@gmail.com>
2023-08-01 14:30:57 +0300

    finished project 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { ProfileCard } from './ProfileCard'
+
 function App() {
-  return <></>
+  return (
+    <>
+      <ProfileCard
+        name='Kristjan'
+        phoneNumber='56565656'
+        age='24'
+        address='best street123'
+      />
+    </>
+  )
 }
 
 export default App
